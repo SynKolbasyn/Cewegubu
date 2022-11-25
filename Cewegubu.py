fc538e66bb3e136b4fa93bce76c3ab4cc9e26066
Andrew <syn.kolbasyn.06@gmail.com>
2022-11-25 19:47:54 +0300

    v0.0.1 Добавлена логика программы и созданы начальные функции

--- Cewegubu.py
+++ Cewegubu.py
@@ -1,1 +1,22 @@
 import account
+
+exit = False
+
+while not(exit):
+    command = input("Введите команду\n>>>   ")
+    print("")
+
+    if (command == "register") or (command == "reg"):
+        account.register()
+
+    elif (command == "login") or (command == "log"):
+        account.login()
+
+    elif command == "help":
+        account.help()
+
+    elif command == "exit":
+        exit = account.exit()
+
+    else:
+        account.unknown()
--- account.py
+++ account.py
@@ -1,1 +1,25 @@
 import server
+
+def end():
+    print("\n" + "-" * 100 + "\n")
+
+def register():
+    print("Вы успешно зарегестрировались")
+    end()
+
+def login():
+    print("Вы успешно вошли в аккаунт")
+    end()
+
+def help():
+    print("Тут список всех команд")
+    end()
+
+def exit():
+    print("Игра овер")
+    end()
+    return True
+
+def unknown():
+    print("Такой команды не существует, используйте help")
+    end()
